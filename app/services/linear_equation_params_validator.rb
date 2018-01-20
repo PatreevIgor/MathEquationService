3a1c93693616c27d0ba171da787dc95c5aa0f56d
Patreev Igor <Patreewigor@yandex.ru>
2018-01-20 23:17:08 +0300

    Add linear equation solver

--- app/services/linear_equation_params_validator.rb
+++ app/services/linear_equation_params_validator.rb
@@ -3,12 +3,31 @@
 class LinearEquationParamsValidator
   include ActiveModel::Model
 
-  attr_accessor :a, :b
+  attr_accessor :a, :b, :params
 
   validates :a, :b, presence: true
   validates :a, :b, numericality: true
+  validate  :equation_has_many_solutions, :equation_has_no_solutions
+
+  def initialize(params)
+    @params = params
+    @a = params[:a]
+    @b = params[:b]
+  end
 
   def valid_params?
     valid?
+  end
+
+  private
+
+  def equation_has_many_solutions
+    return unless params[:a].zero? && params[:b].zero?
+    errors.add(:expiration_date, 'equation has many solutions')
+  end
+
+  def equation_has_no_solutions
+    return unless params[:a].zero? && params[:b] != 0
+    errors.add(:expiration_date, 'equation has no solutions')
   end
 end
