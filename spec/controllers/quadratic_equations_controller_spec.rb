1974df01f4cd50908246e5cde68213d95923754b
Patreev Igor <Patreewigor@yandex.ru>
2018-01-23 12:02:12 +0300

    Add QuadraticEquationsController tests

---
+++ spec/controllers/quadratic_equations_controller_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Api::QuadraticEquationsController do
+  describe '#solve' do
+    before { allow_any_instance_of(described_class).to receive(:params_validator).and_return(params_validator) }
+
+    context 'when params are valid' do
+      let(:params_validator)          { double(:params_validator, valid_params?: true) }
+      let(:quadratic_equation_solver) { double(:quadratic_equation_solver, solve_equation: result) }
+
+      before { allow_any_instance_of(described_class).to receive(:quadratic_equation_solver).and_return(
+               quadratic_equation_solver) }
+
+      context 'when equation has one result' do
+        let(:params) { { a: 1, b: 2, c: -3 } }
+        let(:result) { [1.0,-3.0] }
+
+        it 'responses without errors' do
+          expect(params_validator).to          receive(:valid_params?)
+          expect(quadratic_equation_solver).to receive(:solve_equation)
+
+          post :solve, params: params
+
+          expect(response.body).to eq({ result: result }.to_json)
+          expect(response.code).to eq('200')
+        end
+      end
+
+      context 'when equation has two results' do
+        let(:params) { { a: 1, b: 2, c: 1 } }
+        let(:result) { -1.0 }
+
+        it 'responses without errors' do
+          expect(params_validator).to          receive(:valid_params?)
+          expect(quadratic_equation_solver).to receive(:solve_equation)
+
+          post :solve, params: params
+
+          expect(response.body).to eq({ result: result }.to_json)
+          expect(response.code).to eq('200')
+        end
+      end
+    end
+
+    context 'when params are not valid' do
+      let(:params)           { { a: 1, b: 2, c: 5 } }
+      let(:params_validator) { double(:params_validator, valid_params?: false, errors: errors_object) }
+      let(:errors_object)    { double(:errors_object, full_messages: message_object) }
+      let(:message_object)   { double(:message_object, to_sentence: 'some_message') }
+
+      it 'responses with errors' do
+        expect(params_validator).to receive(:valid_params?)
+
+        post :solve, params: params
+
+        expect(response.body).to eq({ errors: 'some_message' }.to_json)
+        expect(response.code).to eq('200')
+      end
+    end
+  end
+end
