1d46e6a80806b47aed5ed5677f9f5de4933f2cfe
Patreev Igor <Patreewigor@yandex.ru>
2018-01-23 21:40:38 +0300

    Refactor params validator (#13)

--- app/services/linear_equation_params_validator.rb
+++ app/services/linear_equation_params_validator.rb
@@ -1,29 +1,8 @@
 # frozen_string_literal: true
 
-class LinearEquationParamsValidator
-  include ActiveModel::Model
-
+class LinearEquationParamsValidator < EquationParamsValidator
   attr_accessor :a, :b
 
   validates :a, :b, presence: true
   validates :a, :b, numericality: true
-  validate  :equation_has_many_solutions, :equation_has_no_solutions
-
-  def valid_params?
-    valid?
-  end
-
-  private
-
-  def equation_has_many_solutions
-    return unless a.to_f.zero? && b.to_f.zero?
-
-    errors.add(:base, 'equation has many solutions')
-  end
-
-  def equation_has_no_solutions
-    return unless a.to_f.zero? && b.to_f != 0
-
-    errors.add(:base, 'equation does not have solutions')
-  end
 end
--- app/services/quadratic_equation_params_validator.rb
+++ app/services/quadratic_equation_params_validator.rb
@@ -1,28 +1,8 @@
 # frozen_string_literal: true
 
-class QuadraticEquationParamsValidator
-  include ActiveModel::Model
-
+class QuadraticEquationParamsValidator < EquationParamsValidator
   attr_accessor :a, :b, :c
 
   validates :a, :b, :c, presence: true
   validates :a, :b, :c, numericality: true
-  validate  :equation_does_not_have_solutions
-
-  def valid_params?
-    valid?
-  end
-
-  private
-
-  def equation_does_not_have_solutions
-    return if discriminant.positive?
-    return if discriminant.zero?
-
-    errors.add(:base, 'equation does not have solutions')
-  end
-
-  def discriminant
-    (b.to_f**2) - (4 * a.to_f * c.to_f)
-  end
 end
