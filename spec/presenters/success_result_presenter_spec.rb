faa402d079626d98edb1d275588b98eaed3b2b09
Patreev Igor <Patreewigor@yandex.ru>
2018-01-25 19:00:14 +0300

    Update the success result presenter to send the result in friendly form

--- spec/presenters/success_result_presenter_spec.rb
+++ spec/presenters/success_result_presenter_spec.rb
@@ -3,14 +3,34 @@
 require 'rails_helper'
 
 describe SuccessResultPresenter do
-  subject { described_class.new(params) }
+  subject { described_class.new(result) }
 
   describe '#present' do
-    let(:params) { 'some content' }
-    let(:result) { { result: params, code: 0 } }
+    context 'when result equal infiniti' do
+      let(:result)          { Float::INFINITY }
+      let(:friendly_result) { { result: 'Sorry, this equation has infinity solutions', code: 3 } }
 
-    it 'returns result' do
-      expect(subject.present).to eq(result)
+      it 'returns friendly result' do
+        expect(subject.present).to eq(friendly_result)
+      end
+    end
+
+    context 'when result equal nil' do
+      let(:result)          { nil }
+      let(:friendly_result) { { result: 'Sorry, this equation does not have solutions', code: 4 } }
+
+      it 'returns friendly result' do
+        expect(subject.present).to eq(friendly_result)
+      end
+    end
+
+    context 'when result equal some solution' do
+      let(:result)          { 'some solution' }
+      let(:friendly_result) { { result: 'some solution', code: 0 } }
+
+      it 'returns friendly result' do
+        expect(subject.present).to eq(friendly_result)
+      end
     end
   end
 end
